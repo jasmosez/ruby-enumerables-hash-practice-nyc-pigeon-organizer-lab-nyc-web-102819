bcb360bb6cb50eb3995ff6e87861112e29be2403
James Schaffer <jas.schaffer@gmail.com>
2019-09-18 13:31:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,16 @@
 def nyc_pigeon_organizer(data)
-  data.reduce([]) {
+  qualities = []
+  colors = []
+  genders = []
+  locations = []
+  names = []
+  i = 0
+  
+  pigeon_list = data.reduce({}) {
     |new_hash, (key, value)|
-    
-    
+    qualities[i] = key
+    value.reduce
+    i += 1
   }
 end
 
